69544c63978280da01f45b2371e90bf53e3d4702
Kamran Ansari <49868934+kamranansariofficial@users.noreply.github.com>
2025-11-09 12:13:39 +0500

    asd

--- src/app/admin/categories/[slug]/page.js
+++ src/app/admin/categories/[slug]/page.js
@@ -1,19 +1,20 @@
-import Breadcrumbs from '@/layouts/_admin/breadcrumb';
-import EditCategory from '@/components/_admin/categories/edit-category';
+// categories page
+import Breadcrumbs from "@/layouts/_admin/breadcrumb";
+import EditCategory from "@/components/_admin/categories/edit-category";
 
 export default async function Page({ params }) {
   const { slug } = await params;
   return (
-    <div className='space-y-6'>
+    <div className="space-y-6">
       {/* Page Title */}
-      <div className='flex items-center justify-between gap-3'>
+      <div className="flex items-center justify-between gap-3">
         <div>
-          <h1 className='text-2xl font-merriweather'>Redaguoti kategoriją</h1>
+          <h1 className="text-2xl font-merriweather">Redaguoti kategoriją</h1>
           {/* Breadcrumbs */}
           <Breadcrumbs />
         </div>
       </div>
-      <div className='relative'>
+      <div className="relative">
         <EditCategory slug={slug} />
       </div>
     </div>
